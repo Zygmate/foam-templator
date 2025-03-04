aea3c85a2df2accfb0f50e0fb1b0fdf4469bf400
Akito <the@akito.ooo>
2025-03-04 19:20:30 +0100

    Remove Big Pile of Crap

--- packages/foam-vscode/src/features/index.ts
+++ packages/foam-vscode/src/features/index.ts
@@ -1,9 +1,7 @@
 import { FoamFeature } from '../types';
 import * as commands from './commands';
-import * as panels from './panels';
 import dateSnippets from './date-snippets';
 import hoverProvider from './hover-provider';
-import preview from './preview';
 import completionProvider from './link-completion';
 import tagCompletionProvider from './tag-completion';
 import linkDecorations from './document-decorator';
@@ -13,14 +11,12 @@ import refactor from './refactor';
 
 export const features: FoamFeature[] = [
   ...Object.values(commands),
-  ...Object.values(panels),
   refactor,
   navigationProviders,
   wikilinkDiagnostics,
   dateSnippets,
   hoverProvider,
   linkDecorations,
-  preview,
   completionProvider,
   tagCompletionProvider,
 ];
